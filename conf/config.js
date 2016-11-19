b6ef38935dd66e868ad0be769bd3238b06e11f98
Mathias Hedberg <hedberg.mathias@gmail.com>
2016-11-19 20:42:32 +0100

    first commit

---
+++ conf/config.js
@@ -0,0 +1,10 @@
+// DB connection
+var config = {};
+
+config.connAttrs = {
+    "user": "firma-backend",
+    "password": "firma-backend",
+    "connectString": "localhost/XE"
+};
+
+module.exports = config;
